25696723f9ae5b83e32612d592d3fa8f8d1af73c
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-06-25 16:34:07 +0900

    Chore: Vite Config 재설정

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/suspense-example/",
+  plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
