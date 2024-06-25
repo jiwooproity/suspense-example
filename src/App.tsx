f09f30f47103af2414a2e808447bedf6fb396ebc
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-06-25 14:04:29 +0900

    Chore: 기초 파일 구조 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./shared/router";
+
 const App = () => {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
