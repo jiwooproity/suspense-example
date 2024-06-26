27f3f1dbe8d0965700be1a8b27a36f2bb568ee79
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-06-26 12:31:18 +0900

    Deploy: Build and gh-pages Deploy - Vite Config resource base path 설정 - Vite Build minify 및 outdir build 폴더명으로 변경 - Vite gh-pages deploy 폴더 대상 수정 - BrowserRouter Basename 수정 /[repository-name]

--- src/shared/router/index.tsx
+++ src/shared/router/index.tsx
@@ -1,11 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "@/page/main";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Main />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Main />,
+    },
+  ],
+  { basename: "/suspense-example" }
+);
 
 export default router;
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/suspense-example",
   plugins: [react()],
+  server: {
+    open: true,
+  },
+  build: {
+    minify: true,
+    outDir: "./build",
+  },
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
